dce008f9de1fbd758a8d06ea44ffe48c9f685b92
Christine E. Taylor <christine.e.taylor.sf@gmail.com>
2018-03-26 20:15:57 -0700

    streamline display functions

--- clock.js
+++ clock.js
@@ -3,16 +3,17 @@ function clock() {
   let now = new Date();
   let seconds = now.getSeconds();
   let minutes = now.getMinutes();
-  var ampm = 'am';
+  var ampm = 'AM';
   
   let hours = function () {
     let displayHours = now.getHours();
-    if (displayHours > 12) displayHours -=12
+    if (displayHours > 12) {
+      displayHours -=12;
+      ampm = 'PM';
+    }
     if (displayHours < 10) displayHours = '0' + displayHours;
     return displayHours;
   }
-
-  if (now.getHours() > 12) ampm = 'pm'; 
 
   if (now.getMinutes() < 10) minutes = '0' + minutes;
   if (now.getSeconds() < 10) seconds = '0' + seconds;
