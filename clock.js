dacbd8936a69ed9e864f5b0f81baf82270d0ac65
Christine E. Taylor <christine.e.taylor.sf@gmail.com>
2018-03-26 20:12:22 -0700

    improve UI

--- clock.js
+++ clock.js
@@ -7,13 +7,15 @@ function clock() {
   
   let hours = function () {
     let displayHours = now.getHours();
-    if (displayHours > 12) {
-      displayHours -=12
-    }
+    if (displayHours > 12) displayHours -=12
+    if (displayHours < 10) displayHours = '0' + displayHours;
     return displayHours;
   }
 
   if (now.getHours() > 12) ampm = 'pm'; 
+
+  if (now.getMinutes() < 10) minutes = '0' + minutes;
+  if (now.getSeconds() < 10) seconds = '0' + seconds;
 
   document.querySelector('.hours').innerHTML = hours();
   document.querySelector('.minutes').innerHTML = minutes;
